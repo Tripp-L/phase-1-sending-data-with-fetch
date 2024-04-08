fe200e0857fe5b72e9f93a9a04e6a40610ba8ab5
trippl <tlaursen8@yahoo.com>
2024-04-07 22:43:47 -0400

    Sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = {
+    name: "Steve",
+    email: "steve@steve.com"
+}
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData),
+}
+
+return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (res) {
+    return res.json()
+})
+.then(function (data) {
+    const id = data.id
+    document.body.innerHTML += `<p>New ID: ${id}</p>`
+    return id
+})
+.catch(function (error) {
+    document.body.innerHTML += `<p>Error: ${error.message}</p>`
+    })
+}
